b1c44bd069a42eb755ee2a67cddf219d945aff85
Long Nguyen <longnguyen@Longs-MacBook-Air.local>
2023-10-12 16:44:11 -0700

    Studied and applied Tailwind CSS classes to the project

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,11 +1,17 @@
+import CreateUser from '../features/users/CreateUser';
+
 function Home() {
   return (
-    <div>
-      <h1>
-        The best pizza.
+    <div className="my-10 px-4 text-center sm:my-16">
+      <h1 className="text-xl font-semibold mb-8 md:text-3xl">
+        The best pizza
         <br />
-        Straight out of the oven, straight to you.
+        <span className="text-yellow-500">
+          Straight out of the oven, straight to you.
+        </span>
       </h1>
+
+      <CreateUser />
     </div>
   );
 }
