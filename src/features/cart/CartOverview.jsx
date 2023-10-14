58366729dda9932125506d7f47e15baa33c49039
Long Nguyen <longnguyen@Longs-MacBook-Air.local>
2023-10-14 10:40:23 -0700

    Added and practiced Redux and Advanced React Router for fetching data from API

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,11 +1,19 @@
-import { Link } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
+import { formatCurrency } from "../../utils/helpers";
 
 function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity); //state.cart.cart because the state itself called cart and the property also called cart
+  const totalCartPrice = useSelector(getTotalCartPrice);
+
+  if (!totalCartQuantity) return null;
+
   return (
-    <div className="flex items-center justify-between bg-stone-800 text-stone-200 uppercase px-4 py-4 text-sm sm:px-6 md:text-base">
-      <p className="text-stone-300 space-x-4 sm:space-x-6 font-semibold">
-        <span>23 pizzas</span>
-        <span>$23.45</span>
+    <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm uppercase text-stone-200 sm:px-6 md:text-base">
+      <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
+        <span>{totalCartQuantity} pizzas</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
